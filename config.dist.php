68fa20d10dccfbfec01c3e4f147cbbf8ddf1595b
Matthieu Guffroy <mattgu74@gmail.com>
2012-06-20 02:15:59 +0200

    Suppression de plein de truc qui ne servent plus du config.dist

--- config.dist.php
+++ config.dist.php
@@ -1,10 +1,7 @@
 <?php
 
 $CONF = array(
-	"title" => "payutc", 
-	"rechargement_min" => 10.0,  // TODO  NE FAUDRAIT-IL DEMANDER CES VALEURS DIRECTEMENT AU SERVEUR ?
-	"rechargement_max" => 100.0, 
+	"title" => "payutc",
 	"casper_url" => "http://assos.utc.fr/casper/",
-	"cas_url" => "https://cas.utc.fr/cas",
-	"soap_url" => "https://assos.utc.fr/buckutt/SADMIN.class.php?wsdl"
+	"soap_url" => "https://assos.utc.fr/buckutt/MADMIN.class.php?wsdl"
  	);
