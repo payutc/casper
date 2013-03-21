fdc9ba9891c4a4673807340588e353d914129ed8
Arthur Puyou <arthur@puyou.fr>
2013-03-21 01:03:45 +0000

    Il faut l'historique avant son appel

--- templates/footer.php
+++ templates/footer.php
@@ -7,12 +7,12 @@
     </div> <!-- /container -->
     <script src="js/jquery-1.9.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/virement.js"></script>
+    <script src="js/historique.js"></script>
     <script type="text/javascript">
             init();
             selectPage(1);
     </script>
-    <script src="js/virement.js"></script>
-    <script src="js/historique.js"></script>
     </script> 
   </body>
 </html>
