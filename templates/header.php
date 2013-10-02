86f3cc564d5bcf8ce33201f4df878d7bb944796b
Arthur Puyou <arthur@puyou.fr>
2013-10-02 19:17:06 +0200

    Ne pas mettre le bouton déconnexion dans un menu

--- templates/header.php
+++ templates/header.php
@@ -25,13 +25,8 @@
     <div class="navbar navbar-fixed-top navbar-inverse">
       <div class="navbar-inner">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
           <a class="brand" href=""><?php echo $title ?></a>
-          <div class="nav-collapse">
+          <div>
             <p class="navbar-text pull-right"><a href="login">déconnexion</a></p>
           </div>
         </div>
