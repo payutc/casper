52c06d9bc5721465d39c16886c487310712a9a65
Arthur Puyou <arthur@puyou.fr>
2013-10-03 22:13:59 +0200

    Fix missing div et indentation du footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,19 +1,19 @@
-      <hr>
+        </div>
 
-      <footer>
-        <p>&copy; payutc 2012-<?php echo date('Y') ?> – Un problème ou une remarque ? Écris-nous sur <a href="mailto:payutc@assos.utc.fr">payutc@assos.utc.fr</a> !</p>
-      </footer>
+        <hr>
 
-    </div> <!-- /container -->
-    <script src="js/jquery-1.9.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/virement.js"></script>
-    <script src="js/historique.js"></script>
-    <script type="text/javascript">
-            init();
-            selectPage(1);
-            $("a").tooltip();
-    </script>
-    </script> 
-  </body>
+        <footer>
+            <p>&copy; payutc 2012-<?php echo date('Y') ?> – Un problème ou une remarque ? Écris-nous sur <a href="mailto:payutc@assos.utc.fr">payutc@assos.utc.fr</a> !</p>
+        </footer>
+
+        <script src="js/jquery-1.9.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/virement.js"></script>
+        <script src="js/historique.js"></script>
+        <script type="text/javascript">
+        init();
+        selectPage(1);
+        $("a").tooltip();
+        </script>
+    </body>
 </html>
