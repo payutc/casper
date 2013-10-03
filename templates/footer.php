ef54c668a65e86e3928dd09b90c742ad9b472bfa
Arthur Puyou <arthur@puyou.fr>
2013-10-03 14:35:10 +0200

    Adresse mail de payutc

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
       <hr>
 
       <footer>
-        <p>&copy; payutc 2012-<?php echo date('Y') ?></p>
+        <p>&copy; payutc 2012-<?php echo date('Y') ?> – Un problème ou une remarque ? Écris-nous sur <a href="mailto:payutc@assos.utc.fr">payutc@assos.utc.fr</a> !</p>
       </footer>
 
     </div> <!-- /container -->
