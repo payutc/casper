b285059836f1114745cdb9fde19ee371e82c9e5a
Ithorion <antoine_giraud2@hotmail.fr>
2014-06-19 16:19:27 +0200

    Fix collumns position
    Passage au nouveau format de colones du bootstrap v3

--- templates/websale_payutc.php
+++ templates/websale_payutc.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="span4 col-md-offset-4">
+    <div class="col-md-6 col-md-offset-3">
         <?php if(isset($flash['websale_error'])): ?>
             <div class="alert alert-danger"><?php echo $flash['websale_error'] ?></div>
         <?php endif ?>
